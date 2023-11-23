fe1eeda8fe30e6840d37845f2a546634b40d21fe
Arsen <arsen.stakhiv2305@gmail.com>
2023-11-23 22:17:44 +0200

    fixed margins added fit-content

--- src/JS/Header.js
+++ src/JS/Header.js
@@ -8,7 +8,7 @@ function Header(){
     return (
         <header className="bg dark-theme">
             <img className="image" src={logo} alt="logo"></img>
-            <h1 className="big header-footer-text name-view">MathRecall</h1>
+            <h1 className="big header-footer-text name-view" onClick={() => {window.location.reload()}}>MathRecall</h1>
         </header>
     )
 }
